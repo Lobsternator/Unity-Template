95c23264ebcd20f881627516425fa7af1d030fc6
Julius <31416401+Lobsternator@users.noreply.github.com>
2023-04-02 21:27:10 +0200

    SUCCESSFULLY GATHERING STATE MACHINE STATES FUUUUUCCKKKKKKK 😩

--- SourceGenerators/GenerateStateContainer/GenerateStateContainer.cs
+++ SourceGenerators/GenerateStateContainer/GenerateStateContainer.cs
@@ -17,17 +17,14 @@ namespace GenerateStateContainer
         public SyntaxTree SyntaxTree { get; set; }
         public ClassDeclarationSyntax ClassDeclarationSyntax { get; set; }
         public string StateMachineName { get; set; }
-        public string StateName { get; set; }
+        public string BaseStateName { get; set; }
     }
 
     internal class StateSyntax
     {
-        public SyntaxTree SyntaxTree { get; set; }
-
-        public override string ToString()
-        {
-            return SyntaxTree.GetText().ToString();
-        }
+        public string StateMachineName { get; set; }
+        public string BaseStateName { get; set; }
+        public string StateName { get; set; }
     }
 
     [Generator]
@@ -58,11 +55,11 @@ namespace Template.Core
                 SyntaxTree             syntaxTree             = semanticModel.SyntaxTree;
                 ClassDeclarationSyntax classDeclSyntax        = stateContainerSyntax.ClassDeclarationSyntax;
                 string                 stateMachineName       = stateContainerSyntax.StateMachineName;
-                string                 stateName              = stateContainerSyntax.StateName;
+                string                 baseStateName          = stateContainerSyntax.BaseStateName;
                 INamedTypeSymbol       classDeclTypeSymbol    = semanticModel.GetDeclaredSymbol(classDeclSyntax);
                 string                 constituentNamespaces  = string.Join("", classDeclTypeSymbol.ContainingNamespace.ConstituentNamespaces);
 
-                //File.WriteAllText($"C:\\Users\\juliedva\\Desktop\\diag\\debug{i}.txt", string.Join("\n", receiver.States));
+                File.WriteAllText($"C:\\Diagnosis\\debug{i}.txt", string.Join("\n", receiver.States[stateMachineName].Select((s) => s.StateName)));
             }
         }
 
@@ -89,21 +86,23 @@ namespace Template.Core
             if (!(context.Node is ClassDeclarationSyntax))
                 return;
 
-            ClassDeclarationSyntax classDeclarationSyntax = (ClassDeclarationSyntax)context.Node;
-            if (!HasAttribute(classDeclarationSyntax, "GenerateStateContainer"))
+            ClassDeclarationSyntax classDeclSyntax = (ClassDeclarationSyntax)context.Node;
+            if (context.SemanticModel.GetDeclaredSymbol(classDeclSyntax).IsAbstract)
                 return;
 
-            if (!TryGetBaseType(context.SemanticModel, classDeclarationSyntax, "IStateContainer<", out var baseTypeSyntax))
+            if (!HasAttribute(classDeclSyntax, "GenerateStateContainer"))
                 return;
 
+            if (!TryGetBaseType(context.SemanticModel, classDeclSyntax, "IStateContainer<", true, out var baseTypeSymbol))
+                return;
+
+            string[]             typeArgumentNames      = GetTypeArgumentNames(baseTypeSymbol, true);
             StateContainerSyntax stateContainerSyntax   = new StateContainerSyntax();
             stateContainerSyntax.SemanticModel          = context.SemanticModel;
-            stateContainerSyntax.SyntaxTree             = classDeclarationSyntax.SyntaxTree;
-            stateContainerSyntax.ClassDeclarationSyntax = classDeclarationSyntax;
-
-            TypeArgumentListSyntax typeArgumentListSyntax = baseTypeSyntax.DescendantNodes().First((n) => n is TypeArgumentListSyntax) as TypeArgumentListSyntax;
-            stateContainerSyntax.StateMachineName         = typeArgumentListSyntax.Arguments[0].ToString();
-            stateContainerSyntax.StateName                = typeArgumentListSyntax.Arguments.Count > 1 ? typeArgumentListSyntax.Arguments[1].ToString() : $"State<{stateContainerSyntax.StateMachineName}>";
+            stateContainerSyntax.SyntaxTree             = classDeclSyntax.SyntaxTree;
+            stateContainerSyntax.ClassDeclarationSyntax = classDeclSyntax;
+            stateContainerSyntax.StateMachineName       = typeArgumentNames[0];
+            stateContainerSyntax.BaseStateName          = typeArgumentNames.Length > 1 ? typeArgumentNames[1] : $"State<{stateContainerSyntax.StateMachineName}>";
 
             AddStateContainer(stateContainerSyntax);
         }
@@ -113,51 +112,92 @@ namespace Template.Core
             if (!(context.Node is ClassDeclarationSyntax))
                 return;
 
-            ClassDeclarationSyntax classDeclarationSyntax = (ClassDeclarationSyntax)context.Node;
-            if (!TryGetBaseType(context.SemanticModel, classDeclarationSyntax, "State<", out var baseTypeSyntax))
+            ClassDeclarationSyntax classDeclSyntax = (ClassDeclarationSyntax)context.Node;
+            if (context.SemanticModel.GetDeclaredSymbol(classDeclSyntax).IsAbstract)
                 return;
 
-            TypeArgumentListSyntax typeArgumentListSyntax = baseTypeSyntax.DescendantNodes().First((n) => n is TypeArgumentListSyntax) as TypeArgumentListSyntax;
-            string stateMachineName = typeArgumentListSyntax.Arguments[0].ToString();
+            if (!TryGetBaseType(context.SemanticModel, classDeclSyntax, "State<", true, out var baseTypeSymbol))
+                return;
 
-            StateSyntax stateSyntax = new StateSyntax();
-            stateSyntax.SyntaxTree = classDeclarationSyntax.SyntaxTree;
+            string[]    typeArgumentNames = GetTypeArgumentNames(baseTypeSymbol, true);
+            StateSyntax stateSyntax       = new StateSyntax();
+            stateSyntax.StateMachineName  = typeArgumentNames[0];
+            stateSyntax.BaseStateName     = typeArgumentNames.Length > 1 ? typeArgumentNames[1] : $"State<{stateSyntax.StateMachineName}>";
+            stateSyntax.StateName         = classDeclSyntax.Identifier.ToString();
 
-            AddState(stateMachineName, stateSyntax);
+            AddState(stateSyntax);
         }
 
-        private bool HasAttribute(MemberDeclarationSyntax classDeclarationSyntax, string attribute)
+        private bool HasAttribute(MemberDeclarationSyntax classDeclSyntax, string attribute)
         {
-            return classDeclarationSyntax.AttributeLists.Any((a) => a.Attributes.Any((n) => n.ToString().StartsWith(attribute)));
+            return classDeclSyntax.AttributeLists.Any((a) => a.Attributes.Any((n) => n.ToString().StartsWith(attribute)));
         }
 
-        private bool TryGetBaseType(SemanticModel semanticModel, BaseTypeDeclarationSyntax baseTypeDeclSyntax, string baseType, out BaseTypeSyntax baseTypeSyntax)
+        private bool TryGetBaseType(SemanticModel semanticModel, BaseTypeDeclarationSyntax typeDeclSyntax, string baseType, bool includeNamespace, out INamedTypeSymbol baseTypeSymbol)
         {
-            //StreamWriter streamWriter;
-            
-            if (baseTypeDeclSyntax.BaseList is null)
-            {
-                baseTypeSyntax = null;
+            baseTypeSymbol = null;
+
+            if (typeDeclSyntax.BaseList is null)
                 return false;
+
+            foreach (BaseTypeSyntax baseTypeSyntax in typeDeclSyntax.BaseList.Types)
+            {
+                INamedTypeSymbol typeSymbol = semanticModel.GetTypeInfo(baseTypeSyntax.Type).Type as INamedTypeSymbol;
+                if (typeSymbol is null)
+                    continue;
+
+                if (IsDerivedFrom(typeSymbol, baseType, includeNamespace, out baseTypeSymbol))
+                    return true;
             }
 
-            foreach (BaseTypeSyntax t in baseTypeDeclSyntax.BaseList.Types)
+            return false;
+        }
+
+        private bool IsDerivedFrom(INamedTypeSymbol typeSymbol, string targetType, bool includeNamespace, out INamedTypeSymbol baseTypeSymbol)
+        {
+            while (typeSymbol != null)
             {
-                ITypeSymbol typeSymbol = semanticModel.GetTypeInfo(t.Type).Type;
-
-                //streamWriter = File.AppendText("C:\\Users\\juliedva\\Desktop\\diag\\debug0.txt");
-                //streamWriter.WriteLine(string.Join("\n", typeSymbol.AllInterfaces));
-                //streamWriter.Close();
-
-                if (t.ToString().StartsWith(baseType) || typeSymbol.AllInterfaces.Any((i) => i.ToString().StartsWith(baseType)))
+                if (GetFullTypeName(typeSymbol, includeNamespace).StartsWith(targetType))
                 {
-                    baseTypeSyntax = t;
+                    baseTypeSymbol = typeSymbol;
                     return true;
                 }
+
+                typeSymbol = typeSymbol.BaseType;
             }
 
-            baseTypeSyntax = null;
+            baseTypeSymbol = null;
             return false;
+        }
+        private bool IsDerivedFrom(INamedTypeSymbol typeSymbol, string targetType, bool includeNamespace)
+        {
+            return IsDerivedFrom(typeSymbol, targetType, includeNamespace, out var _);
+        }
+
+        private string GetFullTypeName(INamedTypeSymbol typeSymbol, bool includeNamespace)
+        {
+            StringBuilder fullTypeNameBuilder = new StringBuilder();
+            fullTypeNameBuilder.Append(typeSymbol.Name);
+            if (typeSymbol.TypeArguments.Length > 0)
+            {
+                fullTypeNameBuilder.Append("<");
+                fullTypeNameBuilder.Append(string.Join(", ", GetTypeArgumentNames(typeSymbol, includeNamespace)));
+                fullTypeNameBuilder.Append(">");
+            }
+
+            return fullTypeNameBuilder.ToString();
+        }
+
+        private string[] GetTypeArgumentNames(INamedTypeSymbol typeSymbol, bool includeNamespace)
+        {
+            return typeSymbol.TypeArguments.Select((a) =>
+            {
+                string fullArgumentName = a.ToString();
+                if (includeNamespace)
+                    return fullArgumentName;
+
+                return fullArgumentName.Substring(fullArgumentName.LastIndexOf('.') + 1);
+            }).ToArray();
         }
 
         private void AddStateContainer(StateContainerSyntax stateContainerSyntax)
@@ -165,12 +205,12 @@ namespace Template.Core
             StateContainers.Add(stateContainerSyntax);
         }
 
-        private void AddState(string stateMachineName, StateSyntax stateSyntax)
+        private void AddState(StateSyntax stateSyntax)
         {
-            if (States.ContainsKey(stateMachineName))
-                States[stateMachineName].Add(stateSyntax);
+            if (States.ContainsKey(stateSyntax.StateMachineName))
+                States[stateSyntax.StateMachineName].Add(stateSyntax);
             else
-                States.Add(stateMachineName, new List<StateSyntax>() { stateSyntax });
+                States.Add(stateSyntax.StateMachineName, new List<StateSyntax>() { stateSyntax });
         }
     }
 }
